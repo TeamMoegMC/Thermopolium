8c65f808ff73fa63e052e29198c0efc622b12108
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2022-09-02 20:42:53 +0800

    balance and fix

--- src/datagen/java/com/teammoeg/thermopolium/datagen/THPItemTagGenerator.java
+++ src/datagen/java/com/teammoeg/thermopolium/datagen/THPItemTagGenerator.java
@@ -72,7 +72,7 @@ public class THPItemTagGenerator extends TagsProvider<Item> {
 				.addOptional(rl(fd + ":pie_crust"));
 		tag(cereals).addTag(atag(rice)).addTag(ftag("grain")).addTag(atag(baked)).add(Items.WHEAT, Items.WHEAT_SEEDS)
 				.addOptional(rl(RankineItems.CORN_EAR)).addOptional(rl(RankineItems.CORN_FLOUR))
-				.addOptional(rl(RankineItems.CORN_SEEDS)).addTag(atag(frl("bread")));
+				.addOptional(rl(RankineItems.CORN_SEEDS)).addTag(ftag("bread"));
 		tag(rice).addTag(ftag("grain/rice")).addOptional(rl(RankineItems.RICE)).addOptional(rl(RankineItems.RICE_FLOUR))
 				.addOptional(rl(RankineItems.RICE_SEEDS));
 		tag(roots).add(Items.POTATO, Items.BAKED_POTATO).addTag(ftag("rootvegetables"));
@@ -89,7 +89,7 @@ public class THPItemTagGenerator extends TagsProvider<Item> {
 				.addOptional(rl(fd + ":ham")).addOptional(rl(sa + "venison")).addOptional(rl(sa + "auroch_meat"))
 				.addOptional(rl(sa + "mouflon_meat")).addOptional(rl(sa + "boar_meat"))
 				.addOptional(rl(sa + "mammoth_meat")).addOptional(rl(sa + "rhino_meat"))
-				.addOptional(rl(sa + "tiger_meat")).addTag(atag(frl("raw_bacon")));
+				.addOptional(rl(sa + "tiger_meat")).addTag(ftag("raw_bacon"));
 		tag(sugar).add(Items.SUGAR_CANE, Items.HONEYCOMB, Items.HONEY_BOTTLE);
 		tag("bone").add(Items.BONE);
 		tag("ice").add(Items.ICE, Items.BLUE_ICE, Items.PACKED_ICE);
